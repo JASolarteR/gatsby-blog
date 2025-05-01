9a5e6bd61327405e8126d4764c48847f60e32575
JASolarteR <j.solarte@globant.com>
2025-05-01 11:23:38 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "GG Gatsby Blog",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -11,6 +11,13 @@ const config: GatsbyConfig = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
 
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,41 +1,58 @@
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import React, { ReactNode } from "react";
 import {
-    container,
-    heading,
-    navLinks,
-    navLinkItem,
-    navLinkText
-  } from './layout.module.css'
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle,
+} from "./layout.module.css";
 
 type LayoutProps = {
-    pageTitle: string
-    children: ReactNode
-}
+  pageTitle: string;
+  children: ReactNode;
+};
 
-const Layout = ({ pageTitle, children }  : LayoutProps) => {
-    return (
-        <div className={container}>
-          <nav>
-            <ul className={navLinks}>
-              <li className={navLinkItem}>
-                <Link to="/" className={navLinkText}>
-                  Home
-                </Link>
-              </li>
-              <li className={navLinkItem}>
-                <Link to="/about" className={navLinkText}>
-                  About
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <main>
-            <h1 className={heading}>{pageTitle}</h1>
-            {children}
-          </main>
-        </div>
-      )
-  }
+const Layout = ({ pageTitle, children }: LayoutProps) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
 
-export default Layout
+  return (
+    <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
